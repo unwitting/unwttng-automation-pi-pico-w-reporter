9834391fff6f100c55440b190103e99918ecbf2d
Juniper <unwttng@gmail.com>
2023-01-31 23:16:03 +0000

    Update initialise_project_directory.sh

--- initialise_project_directory.sh
+++ initialise_project_directory.sh
@@ -1,4 +1,5 @@
-export CODE_BUNDLE_URL="https://github.com/unwitting/unwttng-automation-pi-pico-w-reporter/archive/refs/heads/main.zip"
+export REPO_NAME="unwttng-automation-pi-pico-w-reporter"
+export CODE_BUNDLE_URL="https://github.com/unwitting/${REPO_NAME}/archive/refs/heads/main.zip"
 export CODE_BUNDLE_FILENAME="code.tar.gz"
 
 echo $CODE_BUNDLE_URL
@@ -17,5 +18,10 @@ tar -xzf "$CODE_BUNDLE_FILENAME"
 
 echo "Removing code bundle..."
 rm "$CODE_BUNDLE_FILENAME"
+
+echo "Expanding directory..."
+mv ${REPO_NAME}/* ./
+mv ${REPO_NAME}/.* ./
+rm -rf ${REPO_NAME}/
 
 echo "Done! Run setup_pi.sh to install firmware and push_to_pi.sh to copy project files."
