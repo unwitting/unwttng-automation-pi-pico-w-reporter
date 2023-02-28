256a51954bbf3214c60f6ecb5c3f340ac450db89
Juniper Preston <unwttng@gmail.com>
2023-02-28 21:57:09 +0000

    s/celcius/celsius

--- sensor_modules/adafruit_stemma_soil/sensor_module_adafruit_stemma_soil.py
+++ sensor_modules/adafruit_stemma_soil/sensor_module_adafruit_stemma_soil.py
@@ -5,5 +5,5 @@ def get_state(i2c):
     sensor = Seesaw(i2c, addr=0x36)
     return [
         ("soil-moisture", sensor.moisture_read(), None),
-        ("soil-temperature", sensor.get_temp(), "celcius"),
+        ("soil-temperature", sensor.get_temp(), "celsius"),
     ]
--- sensor_modules/aht20/sensor_module_aht20.py
+++ sensor_modules/aht20/sensor_module_aht20.py
@@ -4,6 +4,6 @@ import adafruit_ahtx0
 def get_state(i2c):
     sensor = adafruit_ahtx0.AHTx0(i2c)
     return [
-        ("temperature", sensor.temperature, "celcius"),
+        ("temperature", sensor.temperature, "celsius"),
         ("humidity", sensor.relative_humidity, "%"),
     ]
